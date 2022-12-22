82a045054765fec68f70ad943c50a627f12f9ae4
ryu0929miki <118719477+ryu0929miki@users.noreply.github.com>
2022-12-22 18:25:25 +0900

    商品の出品画面〜データ入力保存まで

---
+++ app/models/item.rb
@@ -0,0 +1,34 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :product_name,       presence: true
+  validates :explanation,        presence: true
+  validates :condition_id,       presence: true
+  validates :delivery_charge_id, presence: true
+  validates :shipping_day_id,    presence: true
+  validates :price,              presence: true
+  validates :prefecture_id,      presence: true
+  validates :category_id ,       presence: true
+  validates :image,              presence: true
+
+  with_options numericality: { other_than: 0, message: 'select' } do
+    validates :category_id
+    validates :shipping_day_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :condition_id
+  end
+
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }, inclusion: { in: (300..9_999_999), message: 'Out of setting range' }
+
+
+end
