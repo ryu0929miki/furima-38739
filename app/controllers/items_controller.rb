a4f1161968b528ef9f49f923f57ed56f95f647dd
ryu0929miki <118719477+ryu0929miki@users.noreply.github.com>
2022-12-26 15:35:49 +0900

    商品削除　修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy && current_user.id == @item.user_id
+    @item.destroy
+    if user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     else
       redirect :show
