f58748c5b7d39fbf2d53f78f8cf9662c14878e96
ryu0929miki <118719477+ryu0929miki@users.noreply.github.com>
2022-12-28 15:08:09 +0900

    〜購入内容をデータベースに反映まで

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,31 @@
+class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+  
+  def index
+    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
+    
+    @order_address = OrderAddress.new
+  
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
